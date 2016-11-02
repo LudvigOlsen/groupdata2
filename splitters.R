d2db1f86d6a8075da86374a3eb1aed84fc5ca8ef
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2016-11-02 01:35:36 +0100

    added staircase as method for group_factor

--- splitters.R
+++ splitters.R
@@ -264,7 +264,6 @@ group_factor = function(data, n, method = 'n_windows', force_equal = FALSE, allo
   # Returns a grouping factor
   #
   
-  print(descending)
   
   if(is.data.frame(data)){
     
@@ -279,6 +278,11 @@ group_factor = function(data, n, method = 'n_windows', force_equal = FALSE, allo
     } else if (method == 'n_fill'){
       
       return(n_fill_split_grouping_factor_(data[,1], n, force_equal, allow_zero, descending))
+      
+    } else if (method == 'staircase'){
+      
+      return(stair_split_grouping_factor_(data[,1], n, force_equal, allow_zero, descending))
+      
       
     }
     
@@ -295,6 +299,10 @@ group_factor = function(data, n, method = 'n_windows', force_equal = FALSE, allo
     } else if (method == 'n_fill'){
       
       return(n_fill_split_grouping_factor_(data, n, force_equal, allow_zero, descending))
+      
+    } else if (method == 'staircase'){
+      
+      return(stair_split_grouping_factor_(data, n, force_equal, allow_zero, descending))
       
     }
     
@@ -834,6 +842,100 @@ nv_fill_split_ = function(v, n_windows, force_equal = FALSE, allow_zero = FALSE,
   return(split(v , f = split_grouping_factor))
   
 }
+
+
+# Staircasing 
+
+stair_split_grouping_factor_ = function(v, size, force_equal = FALSE, allow_zero = FALSE, descending = FALSE){
+  
+  # 
+  # Takes a vector and the size of the first window
+  # Returns a staircased grouping factor
+  # .. 1223334444 etc.
+  #
+  
+  # Get the number of groups with no staircasing
+  n_groups = ceiling(length(v)/size)
+  
+  # Create a dataframe with 1 column containing a group index 
+  group_data = data.frame('groups' = c(1:n_groups))
+  
+  
+  # Create a column with number of elements (group number times size)
+  # Create a column with cumulative sum of the number of elements
+  group_data = group_data %>%
+    mutate(n_elements = groups*size,
+           cumsum = cumsum(n_elements))
+  
+  # Get the first row where cumsum is larger or equal to the vector
+  # This contains info on how many groups we need for our staircasing
+  last_group_row = filter(group_data, cumsum >= length(v))[1,]
+
+  # Find how many rows we need for staircasing
+  n_needed_groups = last_group_row[1,1]
+
+  # Get the cumulative sum for that group
+  # This can be used to calculate excess elements
+  # if we include this group in the grouping factor
+  cumsum_last_group = last_group_row[1,3]
+   
+  # Get how many excess elements there are if we
+  # include this group in the grouping factor
+  excess_elements = cumsum_last_group-length(v)
+
+  
+  # If force_equal is set to TRUE
+  if (isTRUE(force_equal)){
+    
+    # If there are any excess elements
+    if (excess_elements > 0){
+      
+      # We will remove the last group
+      group_data = group_data %>%
+        filter(groups <= n_needed_groups-1)
+      
+      # Get the new last row in group_data
+      last_row = tail(group_data, 1)
+      
+      # Get the cumulative sum in the last row
+      cumsum_last_row = last_row[1,3]
+      
+      # Subset the vector to the cumulative sum 
+      # of the last row in group_data
+      # .. So rows 1 to cumulative sum
+      v = head(v, cumsum_last_row)
+    
+    } else {
+      
+      # If there are no excess elements
+      # subset group_data to get the needed groups only
+      group_data = group_data %>%
+        filter(groups <= n_needed_groups)
+      
+    }
+    
+  } else {
+    
+    # If force_equal is set to FALSE
+    # subset group_data to get the needed groups only
+    
+    group_data = group_data %>%
+      filter(groups <= n_needed_groups)
+    
+  }
+  
+  # Create grouping factor 
+  # .. using 'rep(groups, n_elements)'
+  grouping_factor = factor(head(rep(group_data[,1], group_data[,2]), length(v)))
+  
+  # Return grouping factor
+  return(grouping_factor)
+
+  
+}
+
+
+
 
 
 
