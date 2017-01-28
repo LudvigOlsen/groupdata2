e853b02863eea35d8e98c07584ae8ac5c3fc5429
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-01-28 17:48:10 +0100

    Added percentage for partitioning in l_sizes and initialized partition() for balanced partitioning, similar to fold().

--- R/l_methods.R
+++ R/l_methods.R
@@ -8,25 +8,219 @@
 
 # #' @param n List of group sizes
 # #' @export
-l_sizes <- function(n){
+l_sizes <- function(v, n, fixed = FALSE){
+
+  # If any elements in n are between 0-1
+  # .. Convert all wholenumbers in n to percentages (0-1)
+  # ... in a copy of n
+  # .. Calculate for each percentage if it would lead to a group size
+  # ... of at least 1. If not, set the percentage to 0.
+  ### .. If rounded sum of all percentages == 1
+  ### .... Make sure that only length(n) groups are
+  ### .... returned, containg all elements of v
+  # .. Else
+  # .... Let exceeding elements have their own group
+  # ..... Unless of course, specified no to (fixed_n(??) = TRUE)
+  # .. Create list of group sizes
+  # .. Return grouping factor from group sizes
+  # Else just return the given group sizes
+
+  # If any values negative or 0
+  # raise error.
+  if (any(n <= 0)){
+
+    stop("Element <= 0 found in n")
+
+  }
+
+  if (length(n)>length(v)){
+
+    stop("n contains more values than v")
+
+  }
+
+  # Check if any element in n is between 0-1
+  n_perc <- lapply(n, function(x) is_between_(x,0,1)) %>%
+    unlist()
+
+  # If any percentage values in n
+  if (any(n_perc)){
+
+    # Make a copy of n where all elements are in percentage (0-1)
+    n_all_perc <- plyr::llply(1:length(n), function(element){
+
+      ifelse(isTRUE(n_perc[element]),
+             n[element],
+             int_to_perc_(v, n[element])) %>%
+        return()
+
+    }) %>% unlist()
+
+    # To calculate the sum of percentages
+    # We first want to zero out percentages leading to a
+    # group size of 0 because of rounding
+
+    perc_sum_rounded <- n_all_perc %>%
+      { ifelse(round(.*length(v)) > 0,.,0) } %>%
+      sum() %>%
+      round(digits = 2)
+
+    # Check that sum of percentages
+    # is above 0 and below or equal
+    # to 1
+
+    if (perc_sum_rounded > 1){
+
+      stop("Sum of percentages exceedes 1, excluding percentages leading to a group size of 0.")
+
+    } else if (perc_sum_rounded <= 0) {
+
+      stop("Sum of percentages <= 0")
+
+    }
+
+    # Now find the sum of the elements given as whole numbers
+    # We need to make sure that these groups will have the exact number
+    # of values given by the user.
+    #
+    # To recomment:::::
+    # We find the percentage of this sum.
+    # For each of the other percentages we create new percentages reflecting
+    # that we have removed a percentage of the total vector elements
+
+    sum_n_integers <- plyr::llply(1:length(n), function(element){
+
+      ifelse(!isTRUE(n_perc[element]),
+             n[element],
+             0) %>%
+        return()
+
+    }) %>% unlist %>%
+      sum()
+
+    # How many elements are left in percentage?
+    perc_left <- 1 - (sum_n_integers / length(v))
+
+    # How many elements are left?
+    elements_for_perc <- length(v) - sum_n_integers
+
+    # Find group_sizes
+    group_sizes <- plyr::llply(1:length(n), function(element){
+
+      if (isTRUE(n_perc[element])){
+
+        # perc_left can't be 0, because it's the divisor
+        # elements_for_perc
+        ifelse(perc_left < 1, 0,
+               (n[element]/perc_left)*elements_for_perc) %>%
+          return()
+
+      } else {
+
+        n[element] %>% return()
+      }
+
+
+    }) %>% unlist() %>% floor()
+
+  } else {
+
+    #perc_sum_rounded <- 0
+    group_sizes <- n
+
+  }
 
   # n: vector or list of group sizes
 
-  elements <- plyr::llply(1:length(n), function(s){
+  elements <- plyr::llply(1:length(group_sizes), function(s){
 
-    return(rep(s, n[s]))
+    #print(group_sizes[s])
+    return(rep(s, group_sizes[s]))
 
-  })
+  }) %>% unlist()
 
-  return(unlist(elements))
+
+  # Start group number at 1
+  #
+  # if (!isTRUE( all.equal(min(elements), 1))){
+  #
+  #   elements = elements -(min(elements)-1)
+  #
+  #   print('shifted group downwards')
+  # }
+
+  # If fixed is TRUE
+  # . or we have the right number of elements
+  # .. return elements
+  # Else
+  # .. Add a new group with missing elements
+
+
+  if(isTRUE(fixed) || length(elements) == length(v)){
+
+    return(elements)
+
+  } else {
+
+    missing_elements <- length(v) - length(elements)
+
+    elements %>%
+      append(rep(max(elements)+1, missing_elements)) %>%
+      return()
+
+  }
+
+
 }
 
-# system.time(
-#   a <- l_sizes(c(30000,50000,60000, 30000,50000,60000,
-#                  30000,50000,60000, 30000,50000,60000,
-#                  30000,50000,60000, 30000,50000,60000))
-# )
+system.time(
 
+  for (i in 5:100){
+
+    a <- l_sizes(c(1:i), c(0.1,0.4,0.5), fixed = FALSE)
+  }
+)
+
+system.time(l_sizes(c(1:20), c(0.2, 0.2, 0.2), fixed = TRUE))
+
+# Create a balanced partition() function
+
+partition <- function(data, p, cat_col = NULL,
+                    id_col = NULL, fixed = FALSE) {
+
+  #
+  # Balanced partitioning
+  # data: dataframe or vector
+  # p: list of partitions given as percentage (0-1) or group sizes (wholenumber)
+  # cat_col: Categorical variable to balance by
+  # id_col: ID column to keep rows with shared IDs in the same partition
+  # fixed: Whether you only want the inputted partitions or the exceeding values gets a partition (logical)
+  #        FALSE allows you to pass "p = 0.2" and get 2 partions - 0.2 and 0.8
+  #
+
+
+  # Currently not working
+
+  return()
+
+
+
+}
+
+
+
+# test_that("c(1:7) in l_sizes with n = c(0.1,0.2,0.3,0.4)",{
+#
+#   expect_
+#
+#
+# })
+
+# system.time(
+#   a <- l_sizes(c(1:100000),
+#                c(0.2))
+# )
+#a %>% plyr::count()
 
 # method: l_starts
 # Takes values to start groups at
