33b31e64688dd02c638564ea48851063fe82910c
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-02-01 18:17:33 +0100

    Implemented and modified functionality from fold(). Now does balanced partitioning!

--- R/partition.R
+++ R/partition.R
@@ -1,4 +1,4 @@
-partition <- function(data, p, cat_col = NULL,
+partition <- function(data, p = list(0.2),cat_col = NULL,
                       id_col = NULL, fixed = FALSE) {
 
   #
@@ -12,10 +12,74 @@ partition <- function(data, p, cat_col = NULL,
   #
 
 
-  # Currently not working
+  # If cat_col is not NULL
+  if (!is.null(cat_col)){
 
-  return()
+    # If id_col is not NULL
+    if (!is.null(id_col)){
 
+      # Group by cat_col
+      # For each group:
+      # .. create groups of the unique IDs (e.g. subjects)
+      # .. add grouping factor to data
+      # Group by new grouping factor '.partitions'
+
+      data <- data %>%
+        group_by_(cat_col) %>%
+        do(group_uniques_(., n = p, id_col, method = 'l_sizes',
+                          col_name = '.partitions')) %>%
+        group_by_('.partitions')
+
+
+      # If id_col is NULL
+    } else {
+
+      # Group by cat_col
+      # Create groups from data
+      # .. and add grouping factor to data
+
+      data <- data %>%
+        group_by_(cat_col) %>%
+        do(group(., n = p, method = 'l_sizes',
+                 randomize = TRUE,
+                 col_name = '.partitions'))
+
+
+    }
+
+
+    # If cat_col is NULL
+  } else {
+
+    # If id_col is not NULL
+    if (!is.null(id_col)){
+
+      # Create groups of unique IDs
+      # .. and add grouping factor to data
+
+      data <- data %>%
+        group_uniques_(n = p, id_col, method = 'l_sizes',
+                       col_name = '.partitions')
+
+
+      # If id_col is NULL
+    } else {
+
+      # Create groups from all the data points
+      # .. and add grouping factor to data
+
+      data <- group(data, n = p,
+                    method = 'l_sizes',
+                    randomize = TRUE,
+                    col_name = '.partitions')
+
+    }
+
+  }
+
+
+  # Return data
+  return(data)
 
 
 }
