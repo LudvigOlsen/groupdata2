9631365e327d725731e9f39d86cb8bd4f137ffcf
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-03-01 18:23:40 +0100

    Examples

--- R/find_starts.R
+++ R/find_starts.R
@@ -15,29 +15,39 @@
 #' library(groupdata2)
 #'
 #' # Create a dataframe
-#' df <- data.frame('a' = c('a','a','b','b','c','c'))
+#' df <- data.frame('a' = c('a','a','b',
+#'                          'b','c','c'))
 #'
 #' # Get start values for new groups in column 'a'
 #' find_starts(df, col = 'a')
 #'
-#' # Get indices of start values for new groups in column 'a'
-#' find_starts(df, col = 'a', return_index = TRUE)
+#' # Get indices of start values for new groups
+#' # in column 'a'
+#' find_starts(df, col = 'a',
+#'             return_index = TRUE)
 #'
 #' ## Use found starts with l_starts method
+#' # Notice: This is equivalent to n = 'auto'
+#' # with l_starts method
 #'
 #' # Get start values for new groups in column 'a'
 #' starts <- find_starts(df, col = 'a')
 #'
 #' # Use starts in group() with 'l_starts' method
-#' group(df, starts, method = 'l_starts', starts_col = 'a')
+#' group(df, n = starts, method = 'l_starts',
+#'       starts_col = 'a')
 #'
 #' # Similar but with indices instead of values
 #'
-#' # Get indices of start values for new groups in column 'a'
-#' starts_ind <- find_starts(df, col = 'a', return_index = TRUE)
+#' # Get indices of start values for new groups
+#' # in column 'a'
+#' starts_ind <- find_starts(df, col = 'a',
+#'                           return_index = TRUE)
 #'
 #' # Use starts in group() with 'l_starts' method
-#' group(df, starts_ind, method = 'l_starts', starts_col = 'index')
+#' group(df, n = starts_ind, method = 'l_starts',
+#'       starts_col = 'index')
+#'
 #'
 find_starts <- function(data, col = NULL, return_index = FALSE){
 
